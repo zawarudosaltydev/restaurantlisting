7a6665ceff96cab22846ab7fb93b883d3e0e2b12
Oliver Sheng <sryoliver@gmail.com>
2020-03-21 15:30:31 -0400

    set up basic framework

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/zawarudosaltydev/restaurantlisting/controllers"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", controllers.Welcome)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("Failed to start server: %s\n", err.Error())
+		panic(err)
+	}
 }
