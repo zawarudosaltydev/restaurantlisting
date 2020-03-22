c46c3dd8a7def23db51fdf44dc27993651391d18
Ji You <jiy@nulogy.com>
2020-03-22 07:50:12 -0400

    Initialize api endpoint with mockdata

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.13
 
 require (
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
-	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/gorilla/mux v1.7.4
 )
--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
-
+	"github.com/gorilla/mux"
 	"github.com/zawarudosaltydev/restaurantlisting/controllers"
+	"log"
+	"net/http"
 )
 
+// Restaurant Struct
+type Restaurant struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// Init restaurants var as a slice Restaurant struct
+var restaurants []Restaurant
+
+func getRestaurants(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(restaurants)
+}
+
 func main() {
+	// Init Router
+	r := mux.NewRouter()
+
+	// Mock Data - @todo - implement DB
+	restaurants = append(restaurants, Restaurant{ID: "1", Name: "Swatow"})
+
+	// Route Handlers / Endpoints
+	r.HandleFunc("/api/restaurants", getRestaurants).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", r))
+
 	http.HandleFunc("/", controllers.Welcome)
+
 	err := http.ListenAndServe(":8080", nil)
+
 	if err != nil {
 		fmt.Printf("Failed to start server: %s\n", err.Error())
 		panic(err)
