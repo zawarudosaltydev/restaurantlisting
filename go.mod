bd90db06881e747a3f7cf93564d2c6bc22e4f6c8
Ji You <jiy@nulogy.com>
2020-03-22 11:56:14 -0400

    Prefer standard library

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/zawarudosaltydev/restaurantlisting
 
 go 1.13
 
-require (
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
-	github.com/gorilla/mux v1.7.4
-)
+require github.com/go-sql-driver/mysql v1.5.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"github.com/zawarudosaltydev/restaurantlisting/controllers"
-	"log"
 	"net/http"
 )
 
@@ -24,16 +22,11 @@ func getRestaurants(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// Init Router
-	r := mux.NewRouter()
-
 	// Mock Data - @todo - implement DB
 	restaurants = append(restaurants, Restaurant{ID: "1", Name: "Swatow"})
 
 	// Route Handlers / Endpoints
-	r.HandleFunc("/api/restaurants", getRestaurants).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8000", r))
-
+	http.HandleFunc("/api/restaurants", getRestaurants)
 	http.HandleFunc("/", controllers.Welcome)
 
 	err := http.ListenAndServe(":8080", nil)
